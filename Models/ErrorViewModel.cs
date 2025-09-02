12c2d4fe5b22fb86ddb8a9d0a5dfa99efcdfe03c
Anastgerot <anastgerot@gmail.com>
2025-09-02 15:06:41 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gerdt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
