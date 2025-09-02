59903331b22abb56cbadc38c9977598edbe9edb0
Anastgerot <anastgerot@gmail.com>
2025-09-02 15:14:18 +0300

    Создан контроллер

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gerdt.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is Gerdt default action)";
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome()
+        {
+            return "This is the Welcome action method";
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
